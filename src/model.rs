5ad1af957f12a95cf2ea391b9799acfba4351528
Eusef Karl Blancada <blancadaeu@gmail.com>
2026-09-12 23:35:20 +0800

    refactor: modularize current code, add servant model

---
+++ src/model.rs
@@ -0,0 +1,13 @@
+struct Servant {
+    //servant stats
+    name: String,
+    level: u32,
+    max_hp: u32,
+    attack: u32,
+    card: CardType,
+    class: ClassType,
+    attribute: AttributeType,
+    noble_phantasm: NoblePhantasmType,
+    
+
+}
