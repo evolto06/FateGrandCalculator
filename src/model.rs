f353c5614f1e3d1405cf72976610cc161921cc07
Eusef Karl Blancada <blancadaeu@gmail.com>
2026-09-13 22:31:14 +0800

    feat: expand servant model

--- src/model.rs
+++ src/model.rs
@@ -8,6 +8,32 @@ struct Servant {
     class: ClassType,
     attribute: AttributeType,
     noble_phantasm: NoblePhantasmType,
-    
+    skill_1: SkillType,
+    skill_2: SkillType,
+    skill_3: SkillType,
 
+}
+
+impl Servant {
+    fn new(
+        name: String,
+        level: u32,
+        max_hp: u32,
+        attack: u32,
+        card: CardType,
+        class: ClassType,
+        attribute: AttributeType,
+        noble_phantasm: NoblePhantasmType,
+    ) -> Self {
+        Servant {
+            name,
+            level,
+            max_hp,
+            attack,
+            card,
+            class,
+            attribute,
+            noble_phantasm,
+        }
+    }
 }
